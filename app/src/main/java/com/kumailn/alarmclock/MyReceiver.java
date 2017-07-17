dbf41ad0e990b8e8c6531bf60cb28fb6933d7912
Kumail <kumailmn@gmail.com>
2017-07-16 22:47:16 -0400

    -

--- app/src/main/java/com/kumailn/alarmclock/MyReceiver.java
+++ app/src/main/java/com/kumailn/alarmclock/MyReceiver.java
@@ -11,7 +11,7 @@ public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "In Receiver", Toast.LENGTH_SHORT).show();
-        Log.e("In the receiver", "");
+        ///Log.e("In the receiver", "");
         Intent ii = new Intent(context, MyService.class);
         context.startService(ii);
     }
