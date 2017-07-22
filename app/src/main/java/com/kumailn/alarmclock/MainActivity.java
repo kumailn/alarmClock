8af129d3ad967464d5a2b5a89278a8e06de15e4f
Kumail <kumailmn@gmail.com>
2017-07-22 19:29:42 -0400

    Ability to save ringtone for next launch

--- app/src/main/java/com/kumailn/alarmclock/MainActivity.java
+++ app/src/main/java/com/kumailn/alarmclock/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.SystemClock;
@@ -42,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         final Calendar calendar = Calendar.getInstance();
 
-
+        if(!(loadRingtone().equals("NONE"))){
+            myURI = loadRingtone();
+        }
 
 
         checkTime.setOnClickListener(new View.OnClickListener() {
@@ -111,6 +114,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), String.valueOf("Ringtone Set Successfully"), Toast.LENGTH_SHORT).show();
 
             myURI = String.valueOf(selectedfile);
+
+            saveRingtone(myURI);
         }
     }
+
+    public void saveRingtone(String ringtoneURI){
+        SharedPreferences sharedPreferences = getSharedPreferences("myData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("RINGTONE", ringtoneURI);
+        editor.commit();
+    }
+
+    public String loadRingtone(){
+        SharedPreferences sharedPreferences = getSharedPreferences("myData", MODE_PRIVATE);
+        String str = sharedPreferences.getString("RINGTONE", "NONE");
+        return (str);
+    }
+
 }
